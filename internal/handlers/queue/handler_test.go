2bdc94dba57464ca392d847e5269cc417295f5e6
Ivan Timokhov <timokhovim@gmail.com>
2025-03-30 12:35:27 -0300

    Tests: /queue

---
+++ internal/handlers/queue/handler_test.go
@@ -0,0 +1,163 @@
+package queue_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"expat-news/queue-manager/internal/handlers/queue"
+	"expat-news/queue-manager/internal/test_utils"
+	"expat-news/queue-manager/pkg/utils/httpServer"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type whantError struct {
+}
+
+type expected struct {
+	status int
+	body   httpServer.Response
+}
+
+func test_handler_body_eq(a, b httpServer.Response) bool {
+	if a.Code != b.Code {
+		return false
+	}
+	if a.Msg != b.Msg && !strings.HasPrefix(a.Msg, b.Msg) {
+		return false
+	}
+	return true
+}
+
+func TestHandler(t *testing.T) {
+	const target = "/queue"
+	test_utils.SetupDB(t)
+	tests := []struct {
+		req        *http.Request
+		expected   *expected
+		whantError *whantError
+	}{
+		{
+			httptest.NewRequest(http.MethodGet, target+`?publisher=pagina12&state=new`, nil),
+			&expected{
+				http.StatusOK,
+				httpServer.Response{
+					Msg:  `[{"id":3,"publisher":"pagina12","msg":"some post from Pagina 12","state":"new"},{"id":4,"publisher":"pagina12","msg":"some other post from Pagina 12","state":"new"}]`,
+					Code: http.StatusOK,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodGet, target+"?publisher=some", nil),
+			&expected{
+				http.StatusBadRequest,
+				httpServer.Response{
+					Msg:  `Bad Request:`,
+					Code: http.StatusBadRequest,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodGet, target+"?state=new", nil),
+			&expected{
+				http.StatusBadRequest,
+				httpServer.Response{
+					Msg:  `Bad Request:`,
+					Code: http.StatusBadRequest,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodGet, target, nil),
+			&expected{
+				http.StatusBadRequest,
+				httpServer.Response{
+					Msg:  `Bad Request:`,
+					Code: http.StatusBadRequest,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodPost, target, bytes.NewBufferString(`{}`)),
+			&expected{
+				http.StatusBadRequest,
+				httpServer.Response{
+					Msg:  `Bad Request: mising required fields: publisher and/or msgs`,
+					Code: http.StatusBadRequest,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodPost, target, bytes.NewBufferString(`{"publisher":"perfil"}`)),
+			&expected{
+				http.StatusBadRequest,
+				httpServer.Response{
+					Msg:  `Bad Request: mising required fields: publisher and/or msgs`,
+					Code: http.StatusBadRequest,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodPost, target, bytes.NewBufferString(`{"msgs":["some","invalid","data"]}`)),
+			&expected{
+				http.StatusBadRequest,
+				httpServer.Response{
+					Msg:  `Bad Request: mising required fields: publisher and/or msgs`,
+					Code: http.StatusBadRequest,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodPost, target, bytes.NewBufferString(`{"publisher":"perfil","msgs":["some new message"]}`)),
+			&expected{
+				http.StatusCreated,
+				httpServer.Response{
+					Msg:  `1`,
+					Code: http.StatusCreated,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodPost, target, bytes.NewBufferString(`{"publisher":"perfil","msgs":["some other new message","and the third new message"]}`)),
+			&expected{
+				http.StatusCreated,
+				httpServer.Response{
+					Msg:  `2`,
+					Code: http.StatusCreated,
+				},
+			},
+			nil,
+		},
+	}
+
+	for i, test := range tests {
+		test.req.Header.Set("Content-Type", "application/json")
+		w := httptest.NewRecorder()
+		queue.Handler(w, test.req)
+		res := w.Result()
+		if res.StatusCode != test.expected.status {
+			body, _ := io.ReadAll(res.Body)
+			t.Errorf("test %d: status codes mismatch - expected %d, got %d\nbody:\n%s", i, test.expected.status, res.StatusCode, string(body))
+			continue
+		}
+		var actual httpServer.Response
+		if err := json.NewDecoder(res.Body).Decode(&actual); err != nil {
+			t.Errorf("test %d: unexpected error occured while decoding json: %v", i, err)
+			continue
+		}
+		if !test_handler_body_eq(actual, test.expected.body) {
+			t.Errorf("test %d: body mismatch - expected\n%v\ngot\n%v", i, test.expected.body, actual)
+			continue
+		}
+	}
+}
---
+++ internal/handlers/queue/queue.go
@@ -0,0 +1,106 @@
+package queue
+
+import (
+	"encoding/json"
+	"errors"
+	"expat-news/queue-manager/internal/db"
+	repos "expat-news/queue-manager/internal/repositories"
+	"expat-news/queue-manager/internal/utils"
+	"expat-news/queue-manager/pkg/utils/httpServer"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func insert(q *db.Queue, msgs *[]string) httpServer.Response {
+	added, err := q.AddMessages(msgs)
+	if err != nil {
+		if strings.HasPrefix(err.Error(), "unregistered publisher") {
+			return httpServer.NotFound(err.Error())
+		}
+		return httpServer.InternalServerError(err.Error())
+	}
+	return httpServer.Created(fmt.Sprintf("%d", added))
+}
+
+func get(q *db.Queue) httpServer.Response {
+	msgs, err := q.GetMessages(nil)
+	if err != nil {
+		return httpServer.InternalServerError(err.Error())
+	}
+	if len(msgs) == 0 {
+		return httpServer.NoContent("empty result")
+	}
+	result, err := json.Marshal(msgs)
+	if err != nil {
+		return httpServer.InternalServerError(err.Error())
+	}
+	return httpServer.OK(string(result))
+}
+
+func parseBody(queue *db.Queue, msgs *[]string, body io.ReadCloser) error {
+	defer body.Close()
+	bytes, err := io.ReadAll(body)
+	if err != nil {
+		return err
+	}
+	if len(bytes) == 0 {
+		return errors.New("mising required fields: publisher and/or msgs")
+	} else {
+		var data struct {
+			Msgs      []string `json:"msgs"`
+			Publisher string   `json:"publisher"`
+		}
+		if err := json.Unmarshal(bytes, &data); err != nil {
+			return err
+		}
+		if data.Publisher == "" || len(data.Msgs) == 0 {
+			return errors.New("mising required fields: publisher and/or msgs")
+		}
+		queue.Publisher = data.Publisher
+		queue.State = repos.STATE_NEW
+		*msgs = data.Msgs
+	}
+	return nil
+}
+
+func parseQuery(queue *db.Queue, query url.Values) error {
+	state := query.Get("state")
+	publisher := query.Get("publisher")
+	if state == "" || publisher == "" {
+		return errors.New("state and publisher parameter are mandatory")
+	}
+	queue.State = repos.State_t(state)
+	queue.Publisher = publisher
+	return nil
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var queue db.Queue
+	var response httpServer.Response
+
+	if r.Method == http.MethodGet {
+		if err := parseQuery(&queue, r.URL.Query()); err != nil {
+			utils.SendError(w, httpServer.BadRequest(err.Error()))
+			return
+		}
+		response = get(&queue)
+	} else if r.Method == http.MethodPost {
+		var msgs []string
+		if err := parseBody(&queue, &msgs, r.Body); err != nil {
+			utils.SendError(w, httpServer.BadRequest(err.Error()))
+			return
+		}
+		response = insert(&queue, &msgs)
+	} else {
+		utils.SendError(w, httpServer.MethodNotAllowed(r.Method))
+		return
+	}
+	if response.Code >= 400 {
+		utils.SendError(w, response)
+	} else {
+		utils.Send(w, response)
+	}
+}
