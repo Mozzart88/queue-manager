78eb1bbc6aee2db746f3cfaed0d68716dc283878
Ivan Timokhov <timokhovim@gmail.com>
2025-04-05 23:01:34 -0300

    Done

--- internal/handlers/msg/handler_test.go
+++ internal/handlers/msg/handler_test.go
@@ -53,12 +53,101 @@ func TestHandler(t *testing.T) {
 			nil,
 		},
 		{
+			httptest.NewRequest(
+				http.MethodPost,
+				target,
+				bytes.NewBufferString(`{"publisher":"pagina12","msg":"some new msg"}`),
+			),
+			&expected{
+				http.StatusCreated,
+				httpServer.Response{
+					Msg:  `{"id":7,"publisher":"pagina12","msg":"some new msg","state":"new"}`,
+					Code: http.StatusCreated,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(
+				http.MethodPatch,
+				target,
+				bytes.NewBufferString(`{"id":1,"state":"new"}`),
+			),
+			&expected{
+				http.StatusOK,
+				httpServer.Response{
+					Msg:  `ok`,
+					Code: http.StatusOK,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(
+				http.MethodDelete,
+				target,
+				bytes.NewBufferString(`{"id":1}`),
+			),
+			&expected{
+				http.StatusOK,
+				httpServer.Response{
+					Msg:  `ok`,
+					Code: http.StatusOK,
+				},
+			},
+			nil,
+		},
+	}
+
+	for i, test := range tests {
+		test.req.Header.Set("Content-Type", "application/json")
+		w := httptest.NewRecorder()
+		msg.Handler(w, test.req)
+		res := w.Result()
+		if res.StatusCode != test.expected.status {
+			body, _ := io.ReadAll(res.Body)
+			t.Errorf("test %d: status codes mismatch - expected %d, got %d\nbody:\n%s", i, test.expected.status, res.StatusCode, string(body))
+			continue
+		}
+		var actual httpServer.Response
+		if err := json.NewDecoder(res.Body).Decode(&actual); err != nil {
+			t.Errorf("test %d: unexpected error occured while decoding json: %v", i, err)
+			continue
+		}
+		if !test_msg_handler_body_eq(actual, test.expected.body) {
+			t.Errorf("test %d: body mismatch - expected\n%v\ngot\n%v", i, test.expected.body, actual)
+			continue
+		}
+	}
+}
+
+func TestHandler_negative(t *testing.T) {
+	const target = "/msg"
+	test_utils.SetupDB(t)
+	test_utils.SuppressLogging()
+	tests := []struct {
+		req        *http.Request
+		expected   *expected
+		whantError *whantError
+	}{
+		{
 			httptest.NewRequest(http.MethodGet, target+"?msg=1", nil),
 			&expected{
 				http.StatusBadRequest,
 				httpServer.Response{
 					Msg:  `Bad Request:`,
 					Code: http.StatusBadRequest,
+				},
+			},
+			nil,
+		},
+		{
+			httptest.NewRequest(http.MethodGet, target+"?id=256", nil),
+			&expected{
+				http.StatusNotFound,
+				httpServer.Response{
+					Msg:  `Not Found:`,
+					Code: http.StatusNotFound,
 				},
 			},
 			nil,
